09f5d0dc63eb89e34d115b7455c0d3ed5f91c551
GGR <55378169+GGuidiRontani@users.noreply.github.com>
2024-08-23 10:13:11 +0200

    Update cc_merge_meshes_wilcard.bat

--- cloudcompare/cc_merge_meshes_wilcard.bat
+++ cloudcompare/cc_merge_meshes_wilcard.bat
@@ -1,19 +1,26 @@
 REM se déplacer dans le répertoire de travail
-E:
-cd E:\0000_test\2024_cloudcompare_commandline\moche
+REM 	modifier l.3 col.1 et l.4 col.4
+Disk:
+cd path
 
 REM création d'une variable avec pour condition extension obj et incluant déjà l'option global shift
+REM 	modifier l.10 col.15
 setlocal EnableDelayedExpansion
 
-for %%i in (*.obj) do set "files=-o -GLOBAL_SHIFT AUTO "%%i" !files!"
+for %%i in (*.ext) do set "files=-o -GLOBAL_SHIFT AUTO "%%i" !files!"
 
 REM l.14-20 commande de fusion avec CloudCompare
 REM l.14 attention, chemin absolu vers l'executable
-REM l.16 untilisation de la variable
-D:\applications\cloudcompare\2_x_alpha\CloudCompare.exe ^
+REM 	modifier l.16 col.1
+REM l.16 utilisation de la variable
+REM l.22 appel de l'extension en sortie (par ex. OBJ)
+REM 	modifier l.24 col.15
+REM l.24 chemin absolu vers l'output
+REM 	modifier l.26 col.20
+path\CloudCompare.exe ^
 -SILENT ^
 %files% ^
 -MERGE_MESHES ^
--M_EXPORT_FMT OBJ ^
+-M_EXPORT_FMT EXT ^
 -AUTO_SAVE OFF ^
--SAVE_MESHES file "E:\0000_test\2024_cloudcompare_commandline\test.obj"
+-SAVE_MESHES file "path\test.obj"
